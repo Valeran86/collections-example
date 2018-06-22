feb1451abf9abe965651d11f03d0e48c6ffbc3c8
oranvi <a_or@inbox.ru>
2018-06-22 21:53:38 +0500

    сделал 1 и 2 задачи

--- src/main/java/ru/sbt/collections/Counter1DifferentWords.java
+++ src/main/java/ru/sbt/collections/Counter1DifferentWords.java
@@ -6,6 +6,9 @@ import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.regex.Pattern;
 
 /**
  * Подсчитайте количество различных слов в файле.
@@ -13,9 +16,21 @@ import java.net.URISyntaxException;
 public class Counter1DifferentWords {
 
     public static void main( String[] args ) throws IOException, URISyntaxException {
-        InputStream resourceAsStream = Counter1DifferentWords.class.getResourceAsStream("/ru/sbt/collections/VeryBigText.txt");
-        String lines = IOUtils.toString( resourceAsStream, "UTF8" );
+        int i = 0;
+        InputStream resourceAsStream = Counter3DifferentWords.class.getResourceAsStream("/ru/sbt/collections/VeryBigText.txt");
+        String lines = IOUtils.toString(resourceAsStream, "UTF8");
 
-        System.out.println( lines );
+        Pattern pattern = Pattern.compile("[\n\t,.!?;: ]");
+        ArrayList<String> al = new ArrayList<>();
+        HashSet<String> hs = new HashSet<>();
+
+        for (String temp : pattern.split(lines)) {
+            al.add(temp);
+            hs.add(temp);
+        }
+
+        System.out.println("\n" + "количество уникальных слов " + hs.size() + "/общее количество слов с повторениями " + al.size());
     }
 }
+
+
